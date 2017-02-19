84b4a3f2673fb3cae1e00daafaf67da1154317cd
kamleshmuthu <kamleshmuthu05061996@gmail.com>
2017-02-19 14:23:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tourist_guide;
+
+/**
+ *
+ * @author ARUN
+ */
+public class Main {
+    
+    public static void main(String[] arg){
+        MainPage mp=new MainPage();
+        mp.setVisible(true);
+    }
+    
+}
